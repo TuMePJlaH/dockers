36b4f7dda16b5fdb75a11165c6e99562dc6cd984
Kirill Sadovskov <Sadovskov.K@gmail.com>
2022-12-16 12:20:53 +0700

    modified stablediffusion docker

--- stablediffusion/install.sh
+++ stablediffusion/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-git clone https://github.com/facebookresearch/xformers.git
-cd xformers
-git submodule update --init --recursive
-pip install -r requirements.txt
+git clone https://github.com/Stability-AI/stablediffusion.git
+cd stablediffusion
+#pip install -r requirements.txt
 pip install -e .
 cd ..
 
-git clone https://github.com/Stability-AI/stablediffusion.git
-cd stablediffusion
+git clone https://github.com/facebookresearch/xformers.git
+cd xformers
+git submodule update --init --recursive
 pip install -r requirements.txt
 pip install -e .
 cd ..
